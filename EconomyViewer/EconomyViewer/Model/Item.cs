c1f4cc503ca3dc9915496c221176c67ca09ade37
Vlad Golovatyuk <vladd.golovatyuk@gmail.com>
2022-07-15 22:00:12 +0300

    item model and itemlist

---
+++ EconomyViewer/EconomyViewer/Model/Item.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EconomyViewer.Model;
+internal class Item : ICloneable
+{
+    private static readonly Regex _itemPattern = new Regex(@"(?<Header>.+)\s(?<Count>[0-9]+) шт. - (?<Price>[0-9]+)$");
+    public int ID { get; init; }
+    public string Header { get; init; } = string.Empty;
+    public int Count { get; private set; }
+    public int Price { get; private set; }
+    public int PriceForOne => Price / Count;
+    public string Mod { get; init; } = string.Empty;
+
+    public Item() { }
+    public Item(string header, int count, int price, string mod, int id = 0)
+    {
+        ID = id;
+        Header = header ?? throw new ArgumentNullException(nameof(header));
+        Count = count;
+        Price = price;
+        Mod = mod ?? throw new ArgumentNullException(nameof(mod));
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Item item &&
+               Header == item.Header &&
+               PriceForOne == item.PriceForOne &&
+               Mod == item.Mod;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Header, Count, Price, Mod);
+    }
+    public static Item? FromString(string value, string mod)
+    {
+        if (_itemPattern.IsMatch(value))
+        {
+            GroupCollection? comp = _itemPattern.Match(value).Groups;
+            string header = comp["Header"].Value.TrimStart(' ', '\t');
+            int count = Convert.ToInt32(comp["Count"].Value);
+            int price = Convert.ToInt32(comp["Price"].Value);
+            return new Item(header, count, price, mod);
+        }
+        return null;
+    }
+    public void IncreaseCount(int value)
+    {
+        if (value < 0)
+            throw new InvalidOperationException("Value can not be less than zero.");
+
+        int pricePerOne = PriceForOne;
+        Count += value;
+        Price = pricePerOne * Count;
+    }
+    public void DecreaseCount(int value)
+    {
+        if (value < 0)
+            throw new InvalidOperationException("Value can not be less than zero.");
+        if (value >= Count)
+            throw new InvalidOperationException("Value can not be greater than source item count");
+
+        int pricePerOne = PriceForOne;
+        Count -= value;
+        Price = pricePerOne * Count;
+    }
+    public override string? ToString()
+    {
+        return $"{Header} {Count} шт. - {Price}";
+    }
+
+    public object Clone()
+    {
+        return new Item(Header, Count, Price, Mod, ID);
+    }
+}
---
+++ EconomyViewer/EconomyViewer/Model/ItemList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EconomyViewer.Model;
+internal class ItemList : IEnumerable<Item>, ICollection<Item>
+{
+    private List<Item> _items;
+
+    public Item this[int index]
+    {
+        get => _items[index];
+        set => _items[index] = value;
+    }
+    public ItemList()
+    {
+        _items = new List<Item>();
+    }
+
+    public int Count { get => _items.Count; }
+    public bool IsReadOnly { get => false; }
+
+    public void Add(Item item)
+    {
+        Item? same = _items.FirstOrDefault(i => i.Equals(item));
+
+        if (same == null)
+            _items.Add((Item)item.Clone());
+        else
+            same.IncreaseCount(item.Count);
+    }
+
+    public void Clear()
+    {
+        _items.Clear();
+    }
+
+    public bool Contains(Item item)
+    {
+        return _items.Contains(item);
+    }
+
+    public void CopyTo(Item[] array, int arrayIndex)
+    {
+        _items.CopyTo(array, arrayIndex);
+    }
+
+    public IEnumerator<Item> GetEnumerator()
+    {
+        return _items.GetEnumerator();
+    }
+
+    public bool Remove(Item item)
+    {
+        Item? same = _items.FirstOrDefault(i => i.Equals(item));
+        if (same == null)
+            throw new ArgumentException("Collection does not contains element.");
+
+        if (same.Count == 1 || same.Count == item.Count)
+            return _items.Remove(same);
+
+        if (same.Count > item.Count)
+        {
+            same.DecreaseCount(item.Count);
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
