edb8711a6a6c8af534216bc46c705185a89c3026
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-19 14:47:30 +0300

    singleton for context added
    for single access in whole program

--- EconomyViewer/EconomyViewer/DB/ApplicationContext.cs
+++ EconomyViewer/EconomyViewer/DB/ApplicationContext.cs
@@ -7,11 +7,14 @@ namespace EconomyViewer.DB;
 
 internal class ApplicationContext : DbContext
 {
+    private static ApplicationContext? _instance;
     public DbSet<ItemDto>? Items { get; set; }
-    public ApplicationContext()
+    private ApplicationContext()
     {
         Database.EnsureCreated();
     }
+    public static ApplicationContext Context => _instance ??= new ApplicationContext();
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(@"Data Source=" + Path.GetDirectoryName(Environment.ProcessPath) + $@"\economy.db;");
