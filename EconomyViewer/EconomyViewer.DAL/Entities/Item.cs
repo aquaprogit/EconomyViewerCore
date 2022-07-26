fb1dbfa24990a871497db828cdb91e92821ab5dc
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-26 12:25:37 +0300

    changed default count value of item
    to avoid division by zero

--- EconomyViewer/EconomyViewer.DAL/Entities/Item.cs
+++ EconomyViewer/EconomyViewer.DAL/Entities/Item.cs
@@ -6,7 +6,7 @@ public class Item : ICloneable
 {
     private static readonly Regex _itemPattern = new Regex(@"(?<Header>.+)\s(?<Count>[0-9]+) шт. - (?<Price>[0-9]+)$");
     private static readonly Regex _singleItemPattent = new Regex(@"(?<Header>.+)\W+(?<Price>\d+)");
-    private int _count;
+    private int _count = 1;
 
     public int ID { get; init; }
     public string Header { get; init; } = string.Empty;
