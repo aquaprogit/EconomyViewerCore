ea22c66627358543d22f56f1e4a113e90be4ff43
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-26 08:51:20 +0300

    minor changes

--- EconomyViewer/EconomyViewer/Model/Item.cs
+++ EconomyViewer/EconomyViewer/Model/Item.cs
@@ -6,9 +6,18 @@ public class Item : ICloneable
 {
     private static readonly Regex _itemPattern = new Regex(@"(?<Header>.+)\s(?<Count>[0-9]+) шт. - (?<Price>[0-9]+)$");
     private static readonly Regex _singleItemPattent = new Regex(@"(?<Header>.+)\W+(?<Price>\d+)");
+    private int _count;
+
     public int ID { get; init; }
     public string Header { get; init; } = string.Empty;
-    public int Count { get; private set; }
+    public int Count {
+        get => _count;
+        set {
+            int pricePerOne = PriceForOne;
+            _count = value;
+            Price = pricePerOne * Count;
+        }
+    }
     public int Price { get; private set; }
     public int PriceForOne => Price / Count;
     public string Mod { get; init; } = string.Empty;
@@ -48,12 +57,7 @@ public class Item : ICloneable
     }
     public void IncreaseCount(int value)
     {
-        if (value < 0)
-            throw new InvalidOperationException("Value can not be less than zero.");
 
-        int pricePerOne = PriceForOne;
-        Count += value;
-        Price = pricePerOne * Count;
     }
     public void DecreaseCount(int value)
     {
