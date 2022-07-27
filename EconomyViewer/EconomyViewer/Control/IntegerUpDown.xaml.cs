467364bd2d650849b50e562d02ac68ba199684e8
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-27 23:05:04 +0300

    window controls commands

--- EconomyViewer/EconomyViewer/Control/IntegerUpDown.xaml.cs
+++ EconomyViewer/EconomyViewer/Control/IntegerUpDown.xaml.cs
@@ -15,84 +15,83 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EconomyViewer.Control
+namespace EconomyViewer.Control;
+
+/// <summary>
+/// Interaction logic for IntegerUpDown.xaml
+/// </summary>
+public partial class IntegerUpDown : UserControl, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for IntegerUpDown.xaml
-    /// </summary>
-    public partial class IntegerUpDown : UserControl, INotifyPropertyChanged
+    public IntegerUpDown()
     {
-        public IntegerUpDown()
-        {
-            InitializeComponent();
-        }
-        public int Value {
-            get => (int)GetValue(ValueProperty);
-            set {
-                if (value >= MinValue && value <= MaxValue)
-                {
-                    SetValue(ValueProperty, value);
-                    OnPropertyChanged(nameof(IsIncreasable));
-                    OnPropertyChanged(nameof(IsDecreasable));
-                    Debug.WriteLine(Value.ToString());
-                }
-
-            }
-        }
-        public int MaxValue {
-            get => (int)GetValue(MaxValueProperty);
-            set {
-                if (value > MinValue)
-                    SetValue(MaxValueProperty, value);
-                else
-                    throw new ArgumentOutOfRangeException("MaxValue can not be greater than MinValue");
-            }
-        }
-        public int MinValue {
-            get => (int)GetValue(MinValueProperty);
-            set {
-                if (value < MaxValue)
-                    SetValue(MinValueProperty, value);
-                else
-                    throw new ArgumentOutOfRangeException("MinValue can not be less than MinValue");
-            }
-        }
-        public bool IsIncreasable => Value < MaxValue;
-        public bool IsDecreasable => Value > MinValue;
-
-        public static readonly DependencyProperty ValueProperty =
-            DependencyProperty.Register("Value", typeof(int), typeof(IntegerUpDown), new PropertyMetadata(0));
-        public static readonly DependencyProperty MaxValueProperty =
-            DependencyProperty.Register("MaxValue", typeof(int), typeof(IntegerUpDown), new PropertyMetadata(int.MaxValue));
-        public static readonly DependencyProperty MinValueProperty =
-            DependencyProperty.Register("MinValue", typeof(int), typeof(IntegerUpDown), new PropertyMetadata(int.MinValue));
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        private void OnPropertyChanged(string propName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
-        }
-
-        private void UpButton_Click(object sender, RoutedEventArgs e) => Value++;
-
-        private void DownButton_Click(object sender, RoutedEventArgs e) => Value--;
-
-        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) => e.Handled = e.Text.Any(c => char.IsDigit(c) == false);
-
-        private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(string)))
+        InitializeComponent();
+    }
+    public EventHandler<int> ValueChanged { get; set; }
+    public int Value {
+        get => (int)GetValue(ValueProperty);
+        set {
+            if (value >= MinValue && value <= MaxValue)
             {
-                string text = (string)e.DataObject.GetData(typeof(string));
-                if (text.Any(c => char.IsDigit(c) == false))
-                {
-                    e.CancelCommand();
-                }
+                SetValue(ValueProperty, value);
+                ValueChanged(this, value);
+                OnPropertyChanged(nameof(IsIncreasable));
+                OnPropertyChanged(nameof(IsDecreasable));
             }
+        }
+    }
+    public int MaxValue {
+        get => (int)GetValue(MaxValueProperty);
+        set {
+            if (value > MinValue)
+                SetValue(MaxValueProperty, value);
             else
+                throw new ArgumentOutOfRangeException("MaxValue can not be greater than MinValue");
+        }
+    }
+    public int MinValue {
+        get => (int)GetValue(MinValueProperty);
+        set {
+            if (value < MaxValue)
+                SetValue(MinValueProperty, value);
+            else
+                throw new ArgumentOutOfRangeException("MinValue can not be less than MinValue");
+        }
+    }
+    public bool IsIncreasable => Value < MaxValue;
+    public bool IsDecreasable => Value > MinValue;
+
+    public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register("Value", typeof(int), typeof(IntegerUpDown), new PropertyMetadata(0));
+    public static readonly DependencyProperty MaxValueProperty =
+        DependencyProperty.Register("MaxValue", typeof(int), typeof(IntegerUpDown), new PropertyMetadata(int.MaxValue));
+    public static readonly DependencyProperty MinValueProperty =
+        DependencyProperty.Register("MinValue", typeof(int), typeof(IntegerUpDown), new PropertyMetadata(int.MinValue));
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged(string propName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+
+    private void UpButton_Click(object sender, RoutedEventArgs e) => Value++;
+
+    private void DownButton_Click(object sender, RoutedEventArgs e) => Value--;
+
+    private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) => e.Handled = e.Text.Any(c => char.IsDigit(c) == false);
+
+    private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+    {
+        if (e.DataObject.GetDataPresent(typeof(string)))
+        {
+            string text = (string)e.DataObject.GetData(typeof(string));
+            if (text.Any(c => char.IsDigit(c) == false))
             {
                 e.CancelCommand();
             }
+        }
+        else
+        {
+            e.CancelCommand();
         }
     }
 }
--- EconomyViewer/EconomyViewer/MainWindow.xaml.cs
+++ EconomyViewer/EconomyViewer/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Threading;
 
-using EconomyViewer.DAL.EF;
-using EconomyViewer.DAL.ViewModel;
-
-using Microsoft.EntityFrameworkCore;
+using EconomyViewer.MVVM.ViewModel;
 
 namespace EconomyViewer;
 /// <summary>
@@ -15,16 +11,37 @@ namespace EconomyViewer;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly List<Grid> _contentGrids;
     public MainWindow()
     {
         ServerViewModel = new ServerViewModel();
         DataContext = this;
         InitializeComponent();
+        _contentGrids = new() { Main_Grid, Add_Grid, Edit_Grid };
     }
     public ServerViewModel ServerViewModel { get; set; }
+    #region Navigation Handlers
+    private void MinimizeWindow_Button_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+    private void CloseWindow_Button_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+    private void ListViewItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        string menuItem = ((ListViewItem)sender).Tag.ToString()!;
+        _contentGrids.ForEach(contentGrid => contentGrid.Visibility = contentGrid.Name.StartsWith(menuItem) ? Visibility.Visible : Visibility.Hidden);
+    }
+    #endregion
     private void WorkingPanel_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
     {
-
+        ToggleNavBar_ToggleButton.IsChecked = false;
     }
 
     private void ToAdd_Button_Click(object sender, RoutedEventArgs e)
@@ -37,15 +54,8 @@ public partial class MainWindow : Window
 
     }
 
-    private void MinimizeWindow_Button_Click(object sender, RoutedEventArgs e)
-    {
 
-    }
 
-    private void ListViewItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-    {
-
-    }
 
     private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
@@ -82,11 +92,6 @@ public partial class MainWindow : Window
 
     }
 
-    private void CloseWindow_Button_Click(object sender, RoutedEventArgs e)
-    {
-
-    }
-
     private void MainClearToSumUp_Button_Click(object sender, RoutedEventArgs e)
     {
 
@@ -96,4 +101,6 @@ public partial class MainWindow : Window
     {
 
     }
+
+
 }
