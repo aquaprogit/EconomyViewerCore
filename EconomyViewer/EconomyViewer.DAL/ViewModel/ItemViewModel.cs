66509423a3a85bbf60e6d37672feb3d48056eb5b
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-27 17:29:59 +0300

    separated item's logic from serverVM to itemVM

---
+++ EconomyViewer/EconomyViewer.DAL/ViewModel/ItemViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EconomyViewer.DAL.Entities;
+
+namespace EconomyViewer.DAL.ViewModel;
+
+public class ItemViewModel : ViewModelBase
+{
+    private Item _selectedItem;
+    private List<Item>? _items;
+
+    public List<Item>? Items {
+        get => _items;
+        set {
+            _items = value;
+            OnPropertyChanged(nameof(Headers));
+        }
+    }
+    public List<string> Headers => Items?.Select(i => i.Header).ToList() ?? new List<string>();
+    public string SelectedHeader {
+        get => _selectedItem?.Header ?? "";
+        set {
+            if (_selectedItem?.Header == value)
+                return;
+            if (value != null && Headers.Contains(value))
+                SelectedItem = (Item)Items!.First(item => item.Header == value).Clone();
+        }
+    }
+
+    public Item SelectedItem {
+        get => _selectedItem;
+        set {
+            _selectedItem = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(StringFormat));
+            OnPropertyChanged(nameof(Count));
+            OnPropertyChanged(nameof(Price));
+            OnPropertyChanged(nameof(Mod));
+
+        }
+    }
+    public string StringFormat => SelectedItem?.StringFormat ?? "";
+    public int Count {
+        get => SelectedItem?.Count ?? 1;
+        set {
+            SelectedItem.Count = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(Price));
+            OnPropertyChanged(nameof(SelectedItem));
+        }
+    }
+    public int Price => SelectedItem?.Price ?? 0;
+    public string? Mod => SelectedItem?.Mod;
+}
--- EconomyViewer/EconomyViewer.DAL/ViewModel/ServerViewModel.cs
+++ EconomyViewer/EconomyViewer.DAL/ViewModel/ServerViewModel.cs
@@ -11,39 +11,27 @@ namespace EconomyViewer.DAL.ViewModel;
 public class ServerViewModel : ViewModelBase
 {
     private string? _selectedServerName;
-    private string? _selectedItemHeader;
     private List<Server>? Servers => ApplicationContext.Context.Servers?.Include(s => s.Items).ToList();
     private Server? SelectedServer => Servers?.FirstOrDefault(s => s.Name == SelectedServerName);
-    private List<Item>? Items => SelectedServer?.Items;
     public List<string> ServerNames => Servers?.Select(s => s.Name)
                                                .ToList() ?? new List<string>();
-    public List<string> Headers => Items?.Select(i => i.Header).ToList() ?? new List<string>();
-    public Item? SelectedItem => Items?.FirstOrDefault(i => i.Header == SelectedItemHeader);
     public string? SelectedServerName {
         get => _selectedServerName ??= ServerNames.FirstOrDefault();
         set {
             _selectedServerName = value;
             OnPropertyChanged(nameof(SelectedServer));
             OnPropertyChanged(nameof(Mods));
-        }
-    }
-    public string? SelectedItemHeader {
-        get => _selectedItemHeader ??= Headers.First();
-        set {
-            _selectedItemHeader = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(SelectedItem));
-            OnPropertyChanged(nameof(ItemStringFormat));
+            ItemViewModel.Items = SelectedServer?.Items;
         }
     }
     public List<string> Mods => SelectedServer?.Items.Select(s => s.Mod)
                                                      .Distinct()
                                                      .ToList() ?? new List<string>();
-    public string? ItemStringFormat => SelectedItem?.StringFormat;
-
+    public ItemViewModel ItemViewModel { get; set; }
     public ServerViewModel()
     {
         ApplicationContext.Context.SavedChanges += Context_SavedChanges;
+        ItemViewModel = new ItemViewModel() { Items = SelectedServer?.Items };
     }
 
     private void Context_SavedChanges(object? sender, SavedChangesEventArgs e)
