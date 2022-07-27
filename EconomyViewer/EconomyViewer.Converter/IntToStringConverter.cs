4759590ea999f46f599cd37697b32d8e8bda4694
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-27 12:25:35 +0300

    converter for intupdown control

---
+++ EconomyViewer/EconomyViewer.Converter/IntToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EconomyViewer.Converter
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return 0;
+
+            if (value is string str)
+                return System.Convert.ToInt32(str);
+            return 0;
+        }
+    }
+}
