b1c11ec044c8c75fd00d128d63bdb37147a80f94
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-28 08:20:43 +0300

    item inpc implementation

--- EconomyViewer/EconomyViewer.DAL/Entities/Item.cs
+++ EconomyViewer/EconomyViewer.DAL/Entities/Item.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace EconomyViewer.DAL.Entities;
-public class Item : ICloneable
+public class Item : INotifyPropertyChanged, ICloneable
 {
     private static readonly Regex _itemPattern = new Regex(@"(?<Header>.+)\s(?<Count>[0-9]+) шт. - (?<Price>[0-9]+)$");
     private static readonly Regex _singleItemPattent = new Regex(@"(?<Header>.+)\W+(?<Price>\d+)");
     private int _count = 1;
 
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string propName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
     public int ID { get; init; }
     public string Header { get; init; } = string.Empty;
     public int Count {
@@ -16,6 +23,8 @@ public class Item : ICloneable
             int pricePerOne = PriceForOne;
             _count = value;
             Price = pricePerOne * Count;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(Price));
         }
     }
     public int Price { get; private set; }
