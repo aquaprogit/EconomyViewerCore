5308ef55d1d4092385bb9dac5e2c55c8ad87b0fa
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-29 10:36:15 +0300

    vm extended: properties for summing up items

---
+++ EconomyViewer/EconomyViewer.MVVM/Command/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace EconomyViewer.MVVM.Command;
+
+public class RelayCommand : ICommand
+{
+    private Predicate<object>? _canExecute;
+    private Action<object>? _execute;
+
+    public RelayCommand(Action<object>? execute, Predicate<object>? canExecute)
+    {
+        _canExecute = canExecute;
+        _execute = execute;
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute != null && _canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        if (CanExecute(parameter) && _execute != null)
+            _execute(parameter!);
+    }
+}
--- EconomyViewer/EconomyViewer.MVVM/ViewModel/ItemViewModel.cs
+++ EconomyViewer/EconomyViewer.MVVM/ViewModel/ItemViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using EconomyViewer.DAL.Entities;
+using EconomyViewer.MVVM.Command;
 
 namespace EconomyViewer.MVVM.ViewModel;
 
@@ -10,14 +11,25 @@ public class ItemViewModel : ViewModelBase
 {
     private Item? _selectedItem;
     private List<Item> _items;
-
     public ItemViewModel(List<Item>? items)
     {
         _items = items ?? throw new ArgumentNullException(nameof(items));
+        ToSumUpItems = new ItemList();
+        RemoveItemCommand = new RelayCommand((obj) => {
+            ToSumUpItems.Remove(ToSumUpItems.Last());
+            OnPropertyChanged(nameof(TotalSum));
+            OnPropertyChanged(nameof(ToSumUpContent));
+        },
+        (obj) => true);
+        AddItemCommand = new RelayCommand((obj) => {
+            ToSumUpItems.Add(SelectedCopy!);
+            OnPropertyChanged(nameof(TotalSum));
+            OnPropertyChanged(nameof(ToSumUpContent));
+        },
+        (obj) => true);
     }
 
     public Item? SelectedCopy { get; private set; }
-
     public List<Item> Items {
         get => _items;
         set {
@@ -26,7 +38,6 @@ public class ItemViewModel : ViewModelBase
         }
     }
     public List<string> Headers => Items?.Select(i => i.Header).ToList() ?? new List<string>();
-
     public string SelectedHeader {
         get => _selectedItem?.Header ?? string.Empty;
         set {
@@ -42,4 +53,9 @@ public class ItemViewModel : ViewModelBase
             }
         }
     }
+    public ItemList ToSumUpItems { get; set; }
+    public string ToSumUpContent => string.Join('\n', ToSumUpItems.Select(i => i.ToString()));
+    public int TotalSum => ToSumUpItems.Sum(i => i.Price);
+    public RelayCommand AddItemCommand { get; }
+    public RelayCommand RemoveItemCommand { get; }
 }
