53970bf33f2fece5ceb2d8313bff94f8ebb2ae16
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-31 01:07:19 +0300

    two way mods binding

---
+++ EconomyViewer/EconomyViewer.MVVM/Helper/CheckBoxData.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EconomyViewer.MVVM.Helper;
+
+public class CheckBoxData : INotifyPropertyChanged
+{
+    private string _header;
+    private bool _isChecked;
+
+    public string Header {
+        get => _header;
+        set {
+            _header = value;
+            OnPropertyChanged();
+        }
+    }
+    public bool IsChecked {
+        get => _isChecked; set {
+            _isChecked = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string propName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+    public override string ToString()
+    {
+        return $"{Header} - {IsChecked.ToString()[0]}";
+    }
+}
--- EconomyViewer/EconomyViewer.MVVM/ViewModel/ServerViewModel.cs
+++ EconomyViewer/EconomyViewer.MVVM/ViewModel/ServerViewModel.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
+using EconomyViewer.MVVM.Helper;
 using EconomyViewer.DAL.EF;
 using EconomyViewer.DAL.Entities;
 
 using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
 
 namespace EconomyViewer.MVVM.ViewModel;
 
 public class ServerViewModel : ViewModelBase
 {
     private string? _selectedServerName;
+    private ObservableCollection<CheckBoxData> _data;
+
     private List<Server>? Servers => ApplicationContext.Context.Servers?.Include(s => s.Items).ToList();
     private Server? SelectedServer => Servers?.FirstOrDefault(s => s.Name == SelectedServerName);
     public List<string> ServerNames => Servers?.Select(s => s.Name)
@@ -21,12 +25,31 @@ public class ServerViewModel : ViewModelBase
             _selectedServerName = value;
             OnPropertyChanged(nameof(SelectedServer));
             OnPropertyChanged(nameof(Mods));
+            OnPropertyChanged(nameof(Data));
             ItemViewModel.Items = SelectedServer?.Items!;
         }
     }
     public List<string> Mods => SelectedServer?.Items.Select(s => s.Mod)
                                                      .Distinct()
                                                      .ToList() ?? new List<string>();
+    public ObservableCollection<CheckBoxData> Data {
+        get {
+            if (_data != null)
+                return _data;
+            ObservableCollection<CheckBoxData> coll = new(Mods.Select(c => new CheckBoxData() { Header = c, IsChecked = false }));
+            coll.ToList().ForEach(i => i.PropertyChanged += (sender, e) => {
+                OnPropertyChanged(nameof(Data));
+                OnPropertyChanged(nameof(SelectedMods));
+            });
+            return _data = coll;
+        }
+        set {
+            _data = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public List<string> SelectedMods => Data.Where(d => d.IsChecked).Select(c => c.Header).ToList();
     public ItemViewModel ItemViewModel { get; set; }
     public ServerViewModel()
     {
