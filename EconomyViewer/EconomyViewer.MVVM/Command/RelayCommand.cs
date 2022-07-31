8354f0e770e71daadf9d1bbd2583dfe8c6d88c1f
aquaprogit <vladd.golovatyuk@gmail.com>
2022-07-31 21:22:22 +0300

    fixed syntax error

--- EconomyViewer/EconomyViewer.MVVM/Command/RelayCommand.cs
+++ EconomyViewer/EconomyViewer.MVVM/Command/RelayCommand.cs
@@ -8,7 +8,7 @@ public class RelayCommand : ICommand
     private readonly Predicate<object?> _canExecute;
     private readonly Action<object?> _execute;
 
-    public RelayCommand(Action<object?> execute, Predicate<object?> canExecute = )
+    public RelayCommand(Action<object?> execute, Predicate<object?> canExecute)
     {
         _canExecute = canExecute;
         _execute = execute;
